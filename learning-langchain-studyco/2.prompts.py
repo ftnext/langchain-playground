3104ebc9bca946e8f1793dd5fb041d6d06f5f556
ftnext <takuyafjp+develop@gmail.com>
2023-06-26 23:00:57 +0900

    [feat] Rewrite StudyCo prompt example for ChatGPT

---
+++ learning-langchain-studyco/2.prompts.py
@@ -0,0 +1,10 @@
+from langchain.prompts import HumanMessagePromptTemplate
+
+template = """\
+次のコマンドの概要を説明してください。
+
+コマンド: {command}
+"""
+
+prompt = HumanMessagePromptTemplate.from_template(template)
+print(prompt.format(command="echo").content)
