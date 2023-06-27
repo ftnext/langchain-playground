97660ac23a4efb068788ad36724bd9d114d73b4f
ftnext <takuyafjp+develop@gmail.com>
2023-06-27 21:21:55 +0900

    [feat] Users can pass question from command line

--- learning-langchain-studyco/1.models.py
+++ learning-langchain-studyco/1.models.py
@@ -1,7 +1,13 @@
+import argparse
+
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
 
+parser = argparse.ArgumentParser()
+parser.add_argument("question")
+args = parser.parse_args()
+
 chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
-result = chat([HumanMessage(content="GPTとして自己紹介してください。日本語で2文程度でお願いします")])
+result = chat([HumanMessage(content=args.question)])
 print(result.content)
